b073d583937e14f9673d9a8ba4de5b5472445755
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-01-29 22:03:20 +0000

    new

--- Sch-projects/flowchatQ3.py
+++ Sch-projects/flowchatQ3.py
@@ -3,5 +3,7 @@ customerCredit = int(input("Enter credit: "))
 incomePerYear = int(input("Enter income per year: "))
 yearofEmployment = int(input("Enter year of employment: "))
 
-if customerCredit >= 650 and incomePerYear >= 500000 and yearofEmployment >= 2:
-   print("Dear" + " "+ cName + "You are eligible for the loan")
+if customerCredit >= 650 and incomePerYear >= 50000 and yearofEmployment >= 2:
+   print("Dear" + " "+ cName + " " + "You are eligible for the loan")
+else:
+    print("Dear" + " "+ cName + " " + "You are not eligible for the loan")
