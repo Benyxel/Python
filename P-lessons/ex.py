0dbd4fecf0e33ca182bd6f02d532736a554ce4ce
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-01-29 22:43:52 +0000

    new

---
+++ P-lessons/ex.py
@@ -0,0 +1,23 @@
+# ex 1
+
+# List(Array)
+numbers = [1, 2, 3]
+numbers2 = [4, 2, 7]
+print(numbers)
+print(numbers2)
+
+strings = ["a", "b", "c"]
+print(strings)
+booleans = [True]
+
+print(booleans)
+print(numbers[0])
+print(strings[2])
+
+# Dictionary (Object)
+
+name_to_hungry =  {"Jack": False, "Jane": True, "Alax": True}
+print(name_to_hungry)
+
+name_to_burgers = {"Emily": 1, "Jenny": 2, "Neel": 3}
+print(name_to_burgers)
