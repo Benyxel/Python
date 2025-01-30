46abfaf415423322defcb0d0ad3d9a225ec73add
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-01-30 22:51:02 +0000

    new

--- P-lessons/ex2.py
+++ P-lessons/ex2.py
@@ -6,4 +6,13 @@ print(" hehehe".lstrip())
 
 print("he@d.com".endswith(".com"))
 
-print("
+print("Mr. Bean".startswith("Mr."))
+print("supercalifragilistic".find("cali"))
+
+print("mississippi".count("i")) 
+
+print("hahahahaha".replace("a","e"))
+
+print("Bob poked Bob's dog".replace("Bob","Jon"))
+
+print({"A":1, "B":2}.keys())
