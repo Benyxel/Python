84192e12856c73431c96f1848ae5db19c7c42b2e
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-01-30 23:24:06 +0000

    new

--- P-lessons/ifStatements.py
+++ P-lessons/ifStatements.py
@@ -2,4 +2,4 @@ name = input("Enter your name: ")
 if name == "Yeboah Bernard":
     print("Hello, Yeboah Bernard wellcome to the system")
 else:
-    print("Hello, " + name + " you are not allowed to access the system")
+    print("Unrecognized user")
