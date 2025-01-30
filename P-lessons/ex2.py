882f778de2007c54b2de462912dba9f0058329bd
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-01-30 23:17:42 +0000

    new

--- P-lessons/ex2.py
+++ P-lessons/ex2.py
@@ -1,6 +1,5 @@
-lawercase  = ("HHAHAHHA")
-print(lawercase.lower())
-
+lowercase = ("HHAHAHHA")
+print(lowercase.lower())
 
 print(" hehehe".lstrip())
 
@@ -9,12 +8,12 @@ print("he@d.com".endswith(".com"))
 print("Mr. Bean".startswith("Mr."))
 print("supercalifragilistic".find("cali"))
 
-print("mississippi".count("i")) 
+print("mississippi".count("i"))
 
-print("hahahahaha".replace("a","e"))
+print("hahahahaha".replace("a", "e"))
 
-print("Bob poked Bob's dog".replace("Bob","Jon"))
+print("Bob poked Bob's dog".replace("Bob", "Jon"))
 
-print({"A":1, "B":2}.keys())
+print({"A": 1, "B": 2}.keys())
 
-print("/".join(["google.com", "mail","hi"]))
+print("/".join(["google.com", "mail", "hi"]))
---
+++ P-lessons/ifStatements.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+if name == "Yeboah Bernard":
+    print("Hello, Yeboah Bernard wellcome to the system")
+else:
+    print("Hello, " + name + " you are not allowed to access the system")
