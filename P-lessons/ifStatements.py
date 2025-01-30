31a0075a2217fbbe6393433eeaea062acc552123
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-01-30 23:29:12 +0000

    new

--- P-lessons/ifStatements.py
+++ P-lessons/ifStatements.py
@@ -3,3 +3,6 @@ if name == "Yeboah Bernard":
     print("Hello, Yeboah Bernard wellcome to the system")
 else:
     print("Unrecognized user")
+# if statements are used to execute code based on a condition.
+
+
