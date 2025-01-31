b9d3f57c316b27e853c0d70f57932852d57d2957
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-01-31 08:01:04 +0000

    new

--- P-lessons/loop.py
+++ P-lessons/loop.py
@@ -1,6 +1,11 @@
 from datetime import datetime
 now = datetime.now()
-nextBirthday =  datetime(datetime.now().year, 1, 30)
+nextBirthday =  datetime(datetime.now().year, 1, 31)
 if now > nextBirthday:
-    nextBirthday = datetime(datetime.now().year + 1, 1, 30)
+    nextBirthday = datetime(datetime.now().year + 1, 1, 31)
 print(nextBirthday)
+
+# A while loop
+while datetime.now() < nextBirthday:
+    print("It is your birthday")
+
