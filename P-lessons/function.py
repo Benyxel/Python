de87353bb82707fdc9f07e45384f523c57441083
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-01 23:40:26 +0000

    new

--- P-lessons/function.py
+++ P-lessons/function.py
@@ -9,17 +9,49 @@ def mul3(x):
 result = mul3(3)
 print(result)
 
+
 def sub3(x):
     return x - 3
 result = sub3(3)
 print(result)
+
 
 def add(x, b):
     return x + b
 result = add(3, 5)
 print(result)
 
-def product(x,y,z):
-    return x , y , z
+
+def prod(x, y):
+    return x * y
+result = prod(3, 5)
+print(result)
+
+
+def product(x, y, z):
+    return x * y * z
 result = product(3, 5, 7)
 print(result)
+
+
+def gt3(x):
+    return x > 3
+result = gt3(1)
+print(result)
+
+
+def lt10(x):
+    return x < 10
+result = lt10(11)
+print(result)
+
+
+def both_gt3(x, y):
+    return x > 3 and y > 3
+result = both_gt3(4, 4)
+print(result)
+
+
+def sum3(list):
+    return sum3(list)
+result = sum3([1, 2, 3])
