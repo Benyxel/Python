0fe3ab5d14550cabb17530bc7947da72196fdb9d
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-01 23:10:44 +0000

    new

--- P-lessons/function.py
+++ P-lessons/function.py
@@ -3,3 +3,23 @@ def square(x):
 result = square(5)
 print(result)
 
+
+def mul3(x):
+    return x * 3
+result = mul3(3)
+print(result)
+
+def sub3(x):
+    return x - 3
+result = sub3(3)
+print(result)
+
+def add(x, b):
+    return x + b
+result = add(3, 5)
+print(result)
+
+def product(x,y,z):
+    return x , y , z
+result = product(3, 5, 7)
+print(result)
