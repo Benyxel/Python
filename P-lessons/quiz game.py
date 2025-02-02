c02a4e5f561c6cf5a53853716fdd8a84f3b83d98
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-02 23:48:48 +0000

    new

--- P-lessons/quiz game.py
+++ P-lessons/quiz game.py
@@ -4,9 +4,24 @@
 print("Welcome to the quiz game!")
 
 playing = input("Do you want to play? ")
-if playing != "yes": 
+if playing.lower() != "yes": 
     print("Goodbye!")
     exit()
-elif playing == "yes":
-    print("Let's start!")
+
+print("Let's start!")
+score = 0
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
     
+answer = input("What does GPU stand for? ") 
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!") 
+    
+print(f"Your score is {score} out of 2")
