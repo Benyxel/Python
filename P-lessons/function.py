4e2a99020640265f6f597bf3d316f2f25f11da50
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-02 23:29:02 +0000

    new

--- P-lessons/function.py
+++ P-lessons/function.py
@@ -1,55 +1,55 @@
-def square(x):
-    return x * x
-result = square(5)
-print(result)
+# def square(x):
+#     return x * x
+# result = square(5)
+# print(result)
 
 
-def mul3(x):
-    return x * 3
-result = mul3(3)
-print(result)
+# def mul3(x):
+#     return x * 3
+# result = mul3(3)
+# print(result)
 
 
-def sub3(x):
-    return x - 3
-result = sub3(3)
-print(result)
+# def sub3(x):
+#     return x - 3
+# result = sub3(3)
+# print(result)
 
 
-def add(x, b):
-    return x + b
-result = add(3, 5)
-print(result)
+# def add(x, b):
+#     return x + b
+# result = add(3, 5)
+# print(result)
 
 
-def prod(x, y):
-    return x * y
-result = prod(3, 5)
-print(result)
+# def prod(x, y):
+#     return x * y
+# result = prod(3, 5)
+# print(result)
 
 
-def product(x, y, z):
-    return x * y * z
-result = product(3, 5, 7)
-print(result)
+# def product(x, y, z):
+#     return x * y * z
+# result = product(3, 5, 7)
+# print(result)
 
 
-def gt3(x):
-    return x > 3
-result = gt3(1)
-print(result)
+# def gt3(x):
+#     return x > 3
+# result = gt3(1)
+# print(result)
 
 
-def lt10(x):
-    return x < 10
-result = lt10(11)
-print(result)
+# def lt10(x):
+#     return x < 10
+# result = lt10(11)
+# print(result)
 
 
-def both_gt3(x, y):
-    return x > 3 and y > 3
-result = both_gt3(4, 4)
-print(result)
+# def both_gt3(x, y):
+#     return x > 3 and y > 3
+# result = both_gt3(4, 4)
+# print(result)
 
 
 # def sum3(lst):
@@ -58,3 +58,10 @@ print(result)
 # print(result)
 
 
+def f(value, values):
+	v = 1
+	values[0] = 44
+a = 3
+v = [1, 2, 3]
+f(a, v)
+print(a, v[0])
---
+++ P-lessons/quiz game.py
@@ -0,0 +1,12 @@
+
+
+
+print("Welcome to the quiz game!")
+
+playing = input("Do you want to play? ")
+if playing != "yes": 
+    print("Goodbye!")
+    exit()
+elif playing == "yes":
+    print("Let's start!")
+    
