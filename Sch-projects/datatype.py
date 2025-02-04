d06b991d39ff683a0e21d584649583f24593f334
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-04 13:05:08 +0000

    new

---
+++ Sch-projects/datatype.py
@@ -0,0 +1,30 @@
+name = "Yeboah boanu Bernard "
+age = 27
+height = 5.9
+weight = 300
+location = "Accra"
+qualification = "Deploma"
+contact = "+233554881200"
+gender = "Male"
+maritalStatus = False
+occupation = "Developer"
+
+print(name)
+print(type(name))
+print()
+
+print(age)
+print(type(age))
+print()
+
+print(height)
+print(type(height))
+print()
+
+print(weight)
+print(type(weight))
+print()
+
+print(location)
+print(type(location))
+print()
