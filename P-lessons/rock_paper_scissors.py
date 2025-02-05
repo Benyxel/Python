c21bd86d52b340a3feae2c439964bc63bef4a645
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-05 22:39:48 +0000

    new

---
+++ P-lessons/rock_paper_scissors.py
@@ -0,0 +1,11 @@
+options = ['rock', 'paper', 'scissors']
+
+while True:
+    user_input = input("Enter rock, paper, scissors or  Q to quit: ").lower()
+    if user_input == "q":
+        break
+    
+    if user_input not in options:
+        continue
+    
+    
