b73f28972ebd3b75fd480606209426a9c6607175
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-05 22:48:01 +0000

    new

--- P-lessons/rock_paper_scissors.py
+++ P-lessons/rock_paper_scissors.py
@@ -1,11 +1,31 @@
+import random
+
 options = ['rock', 'paper', 'scissors']
 
 while True:
-    user_input = input("Enter rock, paper, scissors or  Q to quit: ").lower()
+    user_input = input("Enter rock, paper, scissors or Q to quit: ").lower()
     if user_input == "q":
         break
     
     if user_input not in options:
         continue
     
+    random_number = random.randint(0, 2) 
+    computer_choice = options[random_number]  
+    print(f"Computer chose {computer_choice}")
     
+    if user_input == "rock" and computer_choice == "scissors":
+        print("You win!")
+        break
+    elif user_input == "paper" and computer_choice == "rock":
+        print("You win!")
+        break
+    elif user_input == "scissors" and computer_choice == "paper":
+        print("You win!")
+        break
+    elif user_input == computer_choice:
+        print("It's a tie!")
+        continue
+    else:
+        print("You lose!")
+        break
