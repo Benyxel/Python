a3d92fc315307d474a982cc2cdb631963f5e3092
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-15 13:21:56 +0000

    new

---
+++ Benyxel/sch_projects/Q4.py
@@ -0,0 +1,14 @@
+def count_vowels():
+    user_input = input("Enter a string: ")
+    if user_input == '':
+        print("You did not enter a string.")
+    vowel_count = 0
+    vowels = "aeiou"
+    vowels = vowels.lower()
+    for letter in user_input:
+        if letter == vowels:
+            vowel_count += 1
+            
+    print("Number of vowels:", vowel_count)
+        
+count_vowels()            
