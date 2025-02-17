8730e306047004f8932cd2e49e75eb2bcf61dafd
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-17 03:53:33 +0000

    new

--- web_p/projects/demoapp/urls.py
+++ web_p/projects/demoapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 #now import the views.py file into this code
 from . import views
 urlpatterns=[
-path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('<int:id>/', views.view_student, name='view_student'),
 ]
--- web_p/projects/demoapp/views.py
+++ web_p/projects/demoapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Students
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 
 def index(request):
     return render(request, 'student/index.html', 
     {'students': Students.objects.all()})
+
+def view_student(request, student_id):
+    student = Students.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
