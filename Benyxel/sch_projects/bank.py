952c576aca1b3b718d679db2783f5dc224b5041c
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-04 21:34:45 +0000

    new

--- Benyxel/sch_projects/bank.py
+++ Benyxel/sch_projects/bank.py
@@ -14,14 +14,19 @@ class BankAccount(ABC):
         pass
         
 class SavingAccount(BankAccount):
-    def __init__(self,account_num,account_holder,interest_rate=0.01):
-        super().__init__(self,account_num,account_holder,balance=0)
+    def __init__(self,account_num,account_holder,balance=0,interest_rate = 0.01):
+        BankAccount.__init__(self,account_num,account_holder,balance=0)
         self.interest_rate = interest_rate  
     
     def deposit(self,amount):
-        self.account_bal += amount
-        print(f"{amount}. New Balance is {self.account_bal}")
+        self.account_bal += amount 
+        rate = self.interest_rate * self.account_bal
+        self.account_bal += rate
         
-saving = SavingAccount(000000, "Bernard", 3000)
-saving.deposit(200)
-print(saving)
+        print(f"{amount} Deposited. Interest is {rate} New Balance is {self.account_bal}")
+        
+saving = SavingAccount(23456, "Bernard,")
+saving.deposit(1000)
+saving.deposit(2000)
+
+
