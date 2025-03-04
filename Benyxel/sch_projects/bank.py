dbd6e476379ed56fa6b170f9ce3b84c8fafef692
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-04 23:52:58 +0000

    nwe

--- Benyxel/sch_projects/bank.py
+++ Benyxel/sch_projects/bank.py
@@ -35,13 +35,32 @@ class SavingAccount(BankAccount):
     def balance(self,):
         self.account_bal 
         print(f"Your total balance is {self.account_bal}")
-
+class CheckingAcoount(BankAccount):
+    def __init__(self,account_num,account_holder,balance=0, overdraft=1000):
+        BankAccount.__init__(self,account_num,account_holder,balance=0,)
+        self.limit = overdraft
     
+    def deposit(self,amount):
+        self.account_bal += amount 
+        rate = self.interest_rate * self.account_bal
+        self.account_bal += rate
+        print(f"Dear! {self.account_holder}, be informed that the sum of {amount} GHS has been Credited into your account:{self.account_num}. Interest is {rate} Current balance is {self.account_bal}")
+    
+    def withdraw(self, amount):
+        if self.account_bal < amount + self.limit:
+            print("You have Reach your Daily limit")
+        else:
+            self.account_bal -= amount
+            print(f"Dear! {self.account_holder},\n. be informed that the sum of {amount} GHS has been\n. Debited from your account:{self.account_num}.\n. Current balance is {self.account_bal}")
+
 saving = SavingAccount(23456, "Yeboah B Bernard")
+checking = CheckingAcoount(23456, "Yeboah B Bernard")
 saving.deposit(2000)
 saving.deposit(2000)
-saving.withdraw(3000)
-saving.balance()
+"""saving.withdraw(3000)
+saving.balance()"""
+
+checking.withdraw(5668)
 
 
 
