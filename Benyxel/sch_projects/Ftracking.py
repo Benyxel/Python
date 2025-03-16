ff646f9b891bf545ef0362c9c4edb5afefb23883
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-03-16 00:17:28 +0000

    new

--- Benyxel/sch_projects/Ftracking.py
+++ Benyxel/sch_projects/Ftracking.py
@@ -14,19 +14,34 @@ class AdminUpdate():
 
 
 class UserAdd(AdminUpdate):
-    def __init__(self,status):
-        AdminUpdate.__init__(self,status)
+    def __init__(self,Sender,Item):
+        AdminUpdate.__init__(self,Sender,Item)
         
         
-    def add_tracking(self,trackNum,name,quantity):
+    def addAdd(self,trackNum,name,quantity,product):
         trackNum = trackNum
         name = name
         quantity = quantity
-        print(f"Tracking number {trackNum} has been added successfully")
+        product = product
+        self.tracking.append({"TrackingNum":trackNum,"Sender":name,"Quantity":quantity,"Product":product})
+        print(f"Tracking number {trackNum} has been added successfully with {quantity} quantity username {name} ")
         print(self.tracking)
+        
+        
+        
+    def userCheck(self,trackNum):
+        trackNum = trackNum
+        for i in self.tracking:
+            if i["TrackingNum"] == trackNum:
+                print(f"Tracking number {trackNum} is available")
+            else:
+                print(f"Tracking number {trackNum} is not available")
                 
                 
                 
-'''adding = AdminUpdate("Kwabena","Laptop",)
+adding = AdminUpdate("Kwabena","Laptop",)
+adding = UserAdd("kwabena","car")
 adding.add_tracking("12645", "Shipped")
-adding.add_tracking("35677" ,"AT the warehouse")'''
+
+
+adding.addAdd("12645","Bernard",1,"Laptop")
