1f9b0e31f50fdffb9fbae37cfa3b0a8c04398185
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-06-05 13:51:50 +0000

    new

---
+++ Benyxel/sch_projects/java/try.js
@@ -0,0 +1,15 @@
+let name_1 = "Kofi";
+let age_1 = 26
+let name_2 = "Kwame" ;
+let age_2 = 48
+
+// if (age_1 > age_2) {
+//     console.log(`${name_1} is older than ${name_2}`)
+// }
+
+// else {
+//     console.log(`${name_2} is older than ${name_1}`)
+// }
+
+
+age_1 > age_2 ? console.log(`${name_1} is older than ${name_2}`) : console.log(`${name_2} is older than ${name_1}`)
