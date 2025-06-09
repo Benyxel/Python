70f28949dd5ee193d56e3f4a352c8378691f1313
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-06-09 00:32:31 +0000

    new

--- Benyxel/sch_projects/freeCodecamp/java.js
+++ Benyxel/sch_projects/freeCodecamp/java.js
@@ -5,3 +5,7 @@ let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
