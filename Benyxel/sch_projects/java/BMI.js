d7d795c8b07ee0fd57eae39715cf9f6b500e34cd
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-06-13 00:08:03 +0000

    new

--- Benyxel/sch_projects/java/BMI.js
+++ Benyxel/sch_projects/java/BMI.js
@@ -1,34 +1,6 @@
-// Import prompt-sync
-const prompt = require("prompt-sync")();
+const ps = require("prompt-sync");
 
-function calculateBMI() {
-  // Get user input using prompt
-  let weight = prompt("Enter your weight in kg: ");
-  let height = prompt("Enter your height in meters: ");
+ps();
 
-  // Convert strings to numbers
-  weight = Number(weight);
-  height = Number(height);
+const prompt = ps({sigint});
 
-  // Calculate BMI
-  let bmi = weight / (height * height);
-
-  // Check BMI category
-  let category;
-  if (bmi < 18.5) {
-    category = "Underweight";
-  } else if (bmi < 25) {
-    category = "Normal weight";
-  } else if (bmi < 30) {
-    category = "Overweight";
-  } else {
-    category = "Obese";
-  }
-
-  // Show results in console
-  console.log("\nYour BMI is: " + bmi.toFixed(2));
-  console.log("Category: " + category);
-}
-
-// Run the function
-calculateBMI();
