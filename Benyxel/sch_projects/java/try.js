138b63961b1f84a5e0a447c93d013decbfc921b4
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-06-14 23:22:58 +0000

    new

--- Benyxel/sch_projects/java/try.js
+++ Benyxel/sch_projects/java/try.js
@@ -11,25 +11,46 @@
 //     console.log(`${name_2} is older than ${name_1}`)
 // }
 
-
 // age_1 > age_2 ? console.log(`${name_1} is older than ${name_2}`) : console.log(`${name_2} is older than ${name_1}`)
 
-let data = ["Kwame", "Kofi", 43, 57, "Maame", "Abena", 45, 100]
+// let data = ["Kwame", "Kofi", 43, 57, "Maame", "Abena", 45, 100]
 
-for (let x of data){
-    let ls = String(x);
-    let als = ls.toLowerCase()
-    if( als == "kofi"){
-        console.log(x)
-    }
-    
-}
-    
-for (let x of data){
-    if (typeof x == "string" && x.toLowerCase() == "kofi")
-    console.log( `${typeof x} + ${x}`)
+// for (let x of data){
+//     let ls = String(x);
+//     let als = ls.toLowerCase()
+//     if( als == "kofi"){
+//         console.log(x)
+//     }
+
+// }
+
+// for (let x of data){
+//     if (typeof x == "string" && x.toLowerCase() == "kofi")
+//     console.log( `${typeof x} + ${x}`)
+// }
+
+// for (let x of data){
+//     console.log(x)
+// }
+
+const ps = require("prompt-sync");
+ps();
+const prompt = ps({ sigint: true });
+
+let grade = prompt("Enter your grade: ");
+grade = parseInt(grade); 
+
+function checkResult(grade) {
+  if (isNaN(grade)) {
+    return "Please enter a valid number";
+  }
+
+  if (grade >= 50) {
+    return "Pass";
+  } else {
+    return "Fail";
+  }
 }
 
-for (let x of data){
-    console.log(x)
-}
+let result = checkResult(grade);
+console.log(`Your grade is ${grade} and you ${result}`);
