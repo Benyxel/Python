0df2dd65d8b3a6a9145efc876481a6343300281f
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-06-17 12:06:41 +0000

    new

--- Benyxel/sch_projects/java/rockpapergame.js
+++ Benyxel/sch_projects/java/rockpapergame.js
@@ -11,7 +11,7 @@ let computerMove = "";
 function computer() {
 
   const randomNumber = Math.random();
-  
+
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
     computerMove = "rock";
   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
@@ -22,25 +22,51 @@ function computer() {
   console.log(computerMove);
 }
 
-function playerMove() {
-  let result = ""
-  if (computerMove === "rock") {
-    result = "Tie";
+function playerGame(playerMove) {
+  let result = "";
+  if (playerMove === "rock") {
+    if (computerMove === "rock") {
+      result = "Tie";
+    } else if (computerMove === "paper") {
+      result = "you lose";
+    } else if (computerMove === "scissors") {
+      result = "you win";
+    }
   }
-  else if (computerMove === "paper") {
-    result = ""
-  }
-}
 
+  else if (playerMove === "paper") {
+    if (computerMove === "rock") {
+      result = "you win";
+    } else if (computerMove === "paper") {
+      result = "Tie";
+    } else if (computerMove === "scissors") {
+      result = "you lose";
+    }
+  }
+
+  else if (playerMove === "scissors") {
+    if (computerMove === "rock") {
+      result = "you lose";
+    } else if (computerMove === "paper") {
+      result = "you win";
+    } else if (computerMove === "scissors") {
+      result = "Tie";
+    }
+  }
+  alert(`you picked ${playerMove} and Computer picked ${computerMove}, ${result}`)
+}
 
 function rock() {
   computer();
+  playerGame("rock")
 }
 
 function paper() {
   computer();
+  playerGame("paper")
 }
 
 function scissors() {
   computer();
+  playerGame("scissors")
 }
