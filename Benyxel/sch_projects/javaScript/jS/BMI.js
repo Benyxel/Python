21acd6cc6e1006f2d324a7f0a5d9ba829d3ab0cf
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-06-19 01:07:07 +0000

    new

--- Benyxel/sch_projects/javaScript/jS/BMI.js
+++ Benyxel/sch_projects/javaScript/jS/BMI.js
@@ -1,27 +1,36 @@
-
-
-function BMI() {
- 
-  height = parseFloat(height) / 100; // Convert cm to meters
-  weight = parseFloat(weight);
-
-  let bmi = weight / (height * height);
-
-  let category;
-  if (bmi < 18.5) {
-    category = "Underweight";
-  } else if (bmi >= 18.5 && bmi < 25) {
-    category = "Normal Weight";
-  } else if (bmi >= 25 && bmi < 30) {
-    category = "Overweight";
-    if (category == "Overweight") {
-      console.log(userName + " " + "You need training🤣😁");
-    }
-  } else {
-    category = "Obesity";
-  }
-
-  console.log(`${userName} your BMI is ${bmi.toFixed(2)}, you're ${category}`);
+function next() {
+  let user = document.querySelector("#username").value;
+  user = document.querySelector("#userNames").innerHTML = user;
 }
+next()
 
-BMI();
+
+
+
+
+
+// function BMI() {
+ 
+//   height = parseFloat(height) / 100; // Convert cm to meters
+//   weight = parseFloat(weight);
+
+//   let bmi = weight / (height * height);
+
+//   let category;
+//   if (bmi < 18.5) {
+//     category = "Underweight";
+//   } else if (bmi >= 18.5 && bmi < 25) {
+//     category = "Normal Weight";
+//   } else if (bmi >= 25 && bmi < 30) {
+//     category = "Overweight";
+//     if (category == "Overweight") {
+//       console.log(userName + " " + "You need training🤣😁");
+//     }
+//   } else {
+//     category = "Obesity";
+//   }
+
+//   console.log(`${userName} your BMI is ${bmi.toFixed(2)}, you're ${category}`);
+// }
+
+// BMI();
