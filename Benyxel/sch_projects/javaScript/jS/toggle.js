cca76195c84b1256acf76819d7f9d43876e22612
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-06-25 00:27:03 +0000

    new

--- Benyxel/sch_projects/javaScript/jS/toggle.js
+++ Benyxel/sch_projects/javaScript/jS/toggle.js
@@ -7,3 +7,20 @@ side.classList.add("toggle");
 btn.addEventListener("click", function () {
   side.classList.toggle("toggle");
 });
+
+
+
+
+
+
+edit.addEventListener("click", function () {
+const name = document.querySelector("#name");
+const contact = document.querySelector("#contact");
+const edit = document.querySelector("#edit");
+const isDisabled = !name.disabled
+  edit.textContent = isDisabled ? "Edit" : "Save";
+    
+    name.disabled = !name.disabled;
+    contact.disabled = !contact.disabled;
+
+})
