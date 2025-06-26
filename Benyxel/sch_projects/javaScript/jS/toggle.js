a796950b5e91a77a4acefbf6ca23f54b6ef99764
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-06-26 14:35:25 +0000

    new

--- Benyxel/sch_projects/javaScript/jS/toggle.js
+++ Benyxel/sch_projects/javaScript/jS/toggle.js
@@ -1,28 +1,36 @@
-const btn = document.querySelector("#toggle");
-const side = document.querySelector("#sidebar");
+// const btn = document.querySelector("#toggle");
+// const side = document.querySelector("#sidebar");
 
-// Add toggle class by default to hide sidebar initially
-side.classList.add("toggle");
+// // Add toggle class by default to hide sidebar initially
+// side.classList.add("toggle");
 
-btn.addEventListener("click", function () {
-  side.classList.toggle("toggle");
-});
+// btn.addEventListener("click", function () {
+//   side.classList.toggle("toggle");
+// });
 
 
-// function edits() {
-//   const name = document.querySelector("#name");
-// const contact = document.querySelector("#contact");
-// const edit = document.querySelector("#edit");
-// const isDisabled = !name.disabled
-//   edit.textContent = isDisabled ? "Edit" : "Save";
+// // function edits() {
+// //   const name = document.querySelector("#name");
+// // const contact = document.querySelector("#contact");
+// // const edit = document.querySelector("#edit");
+// // const isDisabled = !name.disabled
+// //   edit.textContent = isDisabled ? "Edit" : "Save";
     
-//     name.disabled = !name.disabled;
-//     contact.disabled = !contact.disabled;
+// //     name.disabled = !name.disabled;
+// //     contact.disabled = !contact.disabled;
 
-// }
+// // }
 
-// const edit = document.querySelector("#edit");
+// // const edit = document.querySelector("#edit");
 
-// edit.addEventListener("click", edits 
+// // edit.addEventListener("click", edits
 
-// )
+// // )
+
+const url = ("https://pokeapi.co/api/v2/pokemon/ditto")
+
+fetch(url)
+  .then((response) => response.json())
+  .then((jres)=>{console.log(jres)})
+
+.catch((error)=>{console.error(error)})
