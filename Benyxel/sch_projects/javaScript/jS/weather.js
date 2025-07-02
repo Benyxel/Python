d9039b516a8dbb6ab0ca6ccfcc581e27edfd433d
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-07-02 00:55:15 +0000

    new

--- Benyxel/sch_projects/javaScript/jS/weather.js
+++ Benyxel/sch_projects/javaScript/jS/weather.js
@@ -1,43 +1,19 @@
 const url =
-  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=d9029d90403d478c6a48ed328dbb75ad";
+  "https://api.openweathermap.org/data/2.5/weather?unit=metric&q=";
 
-// fetch(url)
-//     .then((response) => response.json())
-//     .then((data)=>{console.log(data.main)})
+const apiKey = "d9029d90403d478c6a48ed328dbb75ad"
 
 weather_fetch();
+const search = document.querySelector("#search"),
+searchbtn = document.querySelector("#searchbtn");
 
-async function weather_fetch() {
-    try {
 
-        const city = document.querySelector("#city"),
-        feels = document.querySelector("#feels"),
-            locations = document.querySelector("#locations");
-        
-        locations.addEventListener("click", function () {
-            if (data.cod != "" && data.cod != 200) {
-                alert(data.massage);
-                return
-             }
-            // console.log(data.coord.lon, data.coord.lat)
-    });
-        
-    const response = await fetch(url + locations.value);
-    if (!response.ok) {
-    throw new Error("Could not find the given string");
-    }
-
+async function weather_fetch(city) {
+    const response = await fetch(url + city + `&appid=${apiKey}`);
     const data = await response.json();
-    console.log(data);
-
-    
-    
-   
-
-    city.innerHTML = data.name;
-    feels.innerHTML = data.main.feels_like;
-    // locations.innerHTML = data;
-} catch (error) {
-    console.error(error, "An error occured");
+    console.log(data)
 }
-}
+
+searchbtn.addEventListener("click", () => {
+    weather_fetch(search.value);
+} )
