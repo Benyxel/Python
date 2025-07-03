380e1ff54165136655e1e92c7f3c0566b0b40a1a
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-07-03 00:38:13 +0000

    new

--- Benyxel/sch_projects/javaScript/jS/weather.js
+++ Benyxel/sch_projects/javaScript/jS/weather.js
@@ -1,43 +1,55 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?unit=metric&q=";
+const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const apiKey = "d9029d90403d478c6a48ed328dbb75ad";
 
 weather_fetch();
 const search = document.querySelector("#search"),
-    searchbtn = document.querySelector("#searchbtn"),
-    allcity = document.querySelector("#city"),
+  searchbtn = document.querySelector("#searchbtn"),
+  allcity = document.querySelector("#city"),
   country = document.querySelector("#country"),
   date = document.querySelector("#date"),
-  weatherIcon = document.querySelector(".fa-cloud")
-
-    
+  weatherIcon = document.querySelector("#weather-icon"),
+  des = document.querySelector("#des"),
+  day = document.querySelector("#day"),
+  tem = document.querySelector("#tem");
 
 async function weather_fetch(city) {
-    
   try {
     const response = await fetch(url + city + `&appid=${apiKey}`);
     const data = await response.json();
-    const cityTime = new Date(data.dt * 1000 + (data.timezone * 1000));
+    const cityTime = new Date(data.dt * 1000 + data.timezone * 1000);
 
-      console.log(data);
-      allcity.innerHTML = data.name
+    console.log(data);
+    allcity.innerHTML = data.name;
     country.innerHTML = `(${data.sys.country})`;
-    date.innerHTML = cityTime.toLocaleString('en-US', { 
-    hour: '2-digit', 
-    minute: '2-digit', 
-    month: 'short', 
-    day: 'numeric' 
-});
-// Output example: "07:15, Jul 3"
+    date.innerHTML = cityTime.toLocaleString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      month: "short",
+      day: "numeric",
+    });
+    day.innerHTML = cityTime.toLocaleString("en-US", { weekday: "long" });
+    des.innerHTML = data.weather[0].description;
+    tem.innerHTML = `${data.main.temp.toFixed(0)}°`;
+
+    // Output example: "07:15, Jul 3"
     if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = ""
+      weatherIcon.src = "../CSS/assets/images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "../CSS/assets/images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "../CSS/assets/images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "../CSS/assets/images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "../CSS/assets/images/mist.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "../CSS/assets/images/snow.png";
     }
-      
   } catch (error) {
     console.error(error, "An error occoured");
   }
-} 
+}
 
 searchbtn.addEventListener("click", () => {
-    weather_fetch(search.value);
-   
+  weather_fetch(search.value);
 });
