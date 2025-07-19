e1b58c0f509911552a3f16709874775ad3ab4207
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-07-19 15:27:24 +0000

    new

--- Benyxel/sch_projects/classprojects/js/dental.js
+++ Benyxel/sch_projects/classprojects/js/dental.js
@@ -1,10 +1,8 @@
 const slidebar = document.querySelector("#sidebar");
-const toggle = document.querySelector("#toggle-btn");
+const toggle = document.querySelector(".bi-list");
 
-// Start with navbar hidden (no toggle class initially)
 
 toggle.addEventListener("click", function () {
   slidebar.classList.toggle("toggle");
   
 });
-
