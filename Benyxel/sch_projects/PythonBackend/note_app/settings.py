fc9e834e760015f63a418c7ee10f97b62a4f4d27
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-10-20 23:24:11 +0000

    new

--- Benyxel/sch_projects/PythonBackend/note_app/settings.py
+++ Benyxel/sch_projects/PythonBackend/note_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'note_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'demodb',
+        'USER': 'root',
+        'PASSWORD' : 'GiftyNarh@2025',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
