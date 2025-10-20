f065792bd53895498cc4347a232cbb3071fb43e0
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-10-20 00:57:47 +0000

    new

--- Benyxel/sch_projects/PythonBackend/notes/admin.py
+++ Benyxel/sch_projects/PythonBackend/notes/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, Person
 # Register your models here.
 class NotesCustom(admin.ModelAdmin):
-    list_display = ('id', 'title' , 'note', 'date', 'time','status')
+    list_display = ('title' , 'note', 'date', 'time','status')
 admin.site.register(Note, NotesCustom)
+
+
+class PersonView(admin.ModelAdmin):
+        list_display = ('first_Name','last_Name','username','email','location','contact','date')
+admin.site.register( Person,PersonView)
+
--- Benyxel/sch_projects/PythonBackend/notes/models.py
+++ Benyxel/sch_projects/PythonBackend/notes/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.   
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_Name = models.CharField(max_length=255, null=False, blank=False)
+    last_Name = models.CharField(max_length=255, null=False, blank=False)
+    username =models.CharField(max_length=255, null=False, blank=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
+    location=models.CharField(max_length=255)
+    contact= models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    
 class Note(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(Person, on_delete=models.CASCADE, default=1001010)
     title = models.CharField(max_length=255, null=False, blank=False)
     note = models.TextField()
     date= models.DateField(auto_now_add=True) 
     time = models.TimeField(auto_now_add=True)
     status = models.TextField(max_length=255, null=False, blank=False)
-    
-class Person(models.Model):
-    id = models.AutoField(primary_key=True)
-    first_Name = models.CharField(max_length=255)
-    last_Name = models.CharField(max_length=255)
-    username =models.CharField(max_length=255)
-    email = models.EmailField()
-    location=models.CharField(max_length=255)
-    contact= models.CharField(max_length=255)
-    date = models.DateTimeField(auto_now_add=True)
