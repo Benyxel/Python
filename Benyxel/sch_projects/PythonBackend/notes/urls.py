fbed6ee0c9dfbc0b1ca3dd91901793eccfaccac9
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-10-21 00:35:04 +0000

    new

--- Benyxel/sch_projects/PythonBackend/notes/urls.py
+++ Benyxel/sch_projects/PythonBackend/notes/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home_view') 
+    path('', views.home_view, name='home_view'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout') 
 ]
--- Benyxel/sch_projects/PythonBackend/notes/views.py
+++ Benyxel/sch_projects/PythonBackend/notes/views.py
@@ -5,3 +5,9 @@ from django.contrib import messages
 # Create your views here.
 def home_view(request):
     return render(request,'index.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
