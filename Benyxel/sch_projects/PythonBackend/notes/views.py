f6d04a9b34260be5f64d527bd294e17fa153b36c
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-10-21 01:01:44 +0000

    new

--- Benyxel/sch_projects/PythonBackend/notes/views.py
+++ Benyxel/sch_projects/PythonBackend/notes/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
 def home_view(request):
-    return render(request,'index.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username =username, password= password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have Been logged in')
+            return redirect('home_view')
+        
+        else:
+            messages.success(request, ' There was an Erorr logging in, Please try again ')
+        
+            return redirect('home_view')
+    else:    
+        return render(request,'index.html', {})
+
+
+
 
 def login_user(request):
     pass
