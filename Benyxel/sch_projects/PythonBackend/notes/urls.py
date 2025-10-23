748f8af564d5cd501585f50279cb3d08da3fcf82
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-10-23 00:19:10 +0000

    new

--- Benyxel/sch_projects/PythonBackend/notes/urls.py
+++ Benyxel/sch_projects/PythonBackend/notes/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home_view, name='home_view'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
-     path('register/', views.logout_user, name='register') 
+     path('register/', views.register_user, name='register') 
 ]
