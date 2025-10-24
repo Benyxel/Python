784e199201ed36aef8b83a663a3ea6f8c3286793
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-10-24 23:40:03 +0000

    new

--- Benyxel/sch_projects/LoginSystem/login/views.py
+++ Benyxel/sch_projects/LoginSystem/login/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
 def home(request):
-    return HttpResponse("Hello world")
+    return HttpResponse( "Helloworld")
 
 
 def signup(request):
-    return HttpResponse("signup")
+    return render(request, "signup")
 
 def login(request):
-    return HttpResponse("login")
+    return render(request, "login")
 
 def logout(request):
-    return HttpResponse("logout")
+    return render(request, "logout")
