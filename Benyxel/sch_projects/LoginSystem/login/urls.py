f47ab7b8dff82f9f59ed7a505b4fcc6f9885da24
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-10-24 23:33:34 +0000

    new

--- Benyxel/sch_projects/LoginSystem/login/urls.py
+++ Benyxel/sch_projects/LoginSystem/login/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name ='login')
+    path('', views.home, name ='home'),
+    path('signup', views.signup, name ='signup'),
+    path('login', views.login, name ='login'),
+    path('logout', views.logout, name ='logout')
 ]
--- Benyxel/sch_projects/LoginSystem/login/views.py
+++ Benyxel/sch_projects/LoginSystem/login/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def login_view(request):
+def home(request):
     return HttpResponse("Hello world")
+
+
+def signup(request):
+    return HttpResponse("signup")
+
+def login(request):
+    return HttpResponse("login")
+
+def logout(request):
+    return HttpResponse("logout")
