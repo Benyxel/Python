6ca54426b323241f3196538e82f7ea51c4d234ad
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-10-29 02:17:38 +0000

    new

--- Benyxel/sch_projects/PythonBackend/note_app/settings.py
+++ Benyxel/sch_projects/PythonBackend/note_app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notes',
+    'notes'
+    
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
